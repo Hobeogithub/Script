d870bafc0288b20e7fcaaaae508237965cebac62
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-02-18 18:29:58 +0700

    Update ScriptAr.lua

--- ScriptAr.lua
+++ ScriptAr.lua
@@ -79,8 +79,8 @@ _G.GunSettings = { -- ปืนที่จะทำ
     ['SoulGuitar'] = true
 }
 
-getgenv().Key = "MARU-97ZM-MF1J1-YZMR-KIOCI-TVQO"
-getgenv().id = "599152046523940874"
+getgenv().Key = "MARU-1H70-COPZ2-OE1F-HEI4Q-S1I1"
+getgenv().id = "1206965213862174772"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 
