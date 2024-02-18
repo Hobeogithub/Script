fb8406ce7706d84372021255789a988072489a3c
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-02-18 19:12:34 +0700

    Create Switch.lua

---
+++ Switch.lua
@@ -0,0 +1,44 @@
+_G.Config = {
+    ['Bypass Tp'] = true,
+    ['White Screen'] = false,
+    ['FPS Booster'] = true,
+    ['Rejoin'] = true,
+    ['RAM'] = {
+        ['Enabled'] = true,
+        ['Delay'] = 600
+    },
+
+    ['Fruit'] = {
+        ['Main'] = {"Kitsune-Kitsune","Mammoth-Mammoth"},
+        ['Select Fruit'] = {"Dark-Dark","Magma-Magma","Ghost-Ghost"},
+        ['Dough Awaken'] = true
+    },
+    ['Race'] = {
+        ['Evo'] = 3,
+        ['Enabled'] = false
+    },
+    ['Farm Mastery'] = {
+        ['Melee'] = true,
+        ['Devil Fruit'] = true,
+        ['Sword'] = true,
+        ['Gun'] = false
+    },
+
+    ['Item Main'] = {
+        ['Godhuman'] = true,
+        ['CDK'] = true,
+        ['Soul Guitar'] = false,
+        ['RGB Haki'] = false,
+        ['Shark Anchor'] = false
+    }
+}
+
+script_key="ydxAFFQmwJtKvwIOnjoDBgCiMsnTDWdK";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/smr.lua"))()
+
+getgenv().PVSetting = {
+    key = 'XwkRa-FBWOK_Hobeo-', -- Secret Code
+    DelayUpdate = 300, -- Delay Update (Seconds)
+    note = 'Hobeo', 
+}
+loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
