fc36aeba800e15a244d6f98ffb0a1e30a17a5be4
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-02-22 23:57:02 +0700

    Update Toilet.lua

--- Toilet.lua
+++ Toilet.lua
@@ -1,17 +1,19 @@
 shared.Config = {
     KaitanMode = {
-        ["Enabled"] = false,
+        ["Enabled"] = true,
     },
     Webhooks = {
         ["Enabled"] = false,
         ["URL"] = "",
     },
     Settings = {
-        ["Select World"] = "PalmParadise",
-        ['Vote Difficulty'] = 'Nightmare', -- Easy, Medium, Hard, Nightmare
+        ["Select World"] = "ToiletCity",
+        ['Vote Difficulty'] = 'Nightmare',
         ['Auto Replay'] = true,
         ['Auto Skip Wave'] = true,
         ['FPS Booster'] = true,
+        ['White Screen Mode'] = false,
+        ['Hide Name'] = true,
     },
 }
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua')){219206403627}
