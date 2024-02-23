040d9f93c8d80dd874e86d2fbf3478412733fb71
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-02-23 22:19:35 +0700

    Update 2.3-2.4.lua

--- 2.3-2.4.lua
+++ 2.3-2.4.lua
@@ -1,92 +1,22 @@
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
-_G.Team = "Pirate" -- Marine / Pirate
-_G.KAITUN_SCRIPT = true
-_G.LogsDes = {
-    ["Enabled"] = true, -- เปิดการใช้งาน
-    ["SendAlias"] = true, -- เปิดการส่ง Alias
-    ["SendDescription"] = true, -- เปิดการส่ง Des
-    ["DelaySend"] = 5 -- วินาที
+_G.AutoVote = true 
+_G.Difficulty = "Nightmare" -- "Easy" / "Medium" / "Hard" / "Nightmare" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CodexHubX/e32cqvvx3dz486221wwdac/main/TTDAutoVote.lua"))()
+shared.Config = {
+    KaitanMode = {
+        ["Enabled"] = false,
+    },
+    Webhooks = {
+        ["Enabled"] = true,
+        ["URL"] = "https://discord.com/api/webhooks/1210518410627129374/5wDqyqhfUx72IgGNyUq028p6RkWIBOpA38C9toW5SNTxMdTPrF-9zOeqDjLKEx3oVXI9",
+    },
+    Settings = {
+        ["Select World"] = "ToiletCity",
+        ['Vote Difficulty'] = 'Nightmare',
+        ['Auto Replay'] = true,
+        ['Auto Skip Wave'] = true,
+        ['FPS Booster'] = true,
+        ['White Screen Mode'] = false,
+        ['Hide Name'] = true,
+    },
 }
-_G.MainSettings = {
-        ["EnabledHOP"] = true, -- เปิด HOP ( มันไม่มีอยู่ละใส่มาเท่ๆ )
-        ['FPSBOOST'] = true, -- ภาพกาก
-        ["FPSLOCKAMOUNT"] = 20, -- จำนวน FPS
-        ['WhiteScreen'] = true, -- จอขาว
-        ['CloseUI'] = false, -- ปิด Ui
-        ["NotifycationExPRemove"] = true, -- ลบ ExP ที่เด้งตอนฆ่ามอน
-        ['AFKCheck'] = 150, -- ถ้ายืนนิ่งเกินวิที่ตั้งมันจะรีเกม
-        ["LockFragments"] = 20000, -- ล็อคเงินม่วง
-        ["LockFruitsRaid"] = { -- ล็อคผลที่ไม่เอาไปลงดัน
-            ["Kitsune-Kitsune"] = true,
-            ["Dough-Dough"] = true,
-            ["Leopard-Leopard"] = true,
-            ["Mammoth-Mammoth"] = true,
-            ["T-T"] = true,
-            ["Dragon-Dragon"] = true
-        }
-    }
-_G.Fruits_Settings = { -- ตั้งค่าผล
-    ['Main_Fruits'] = {'Dough-Dough'}, -- ผลหลัก ถ้ายังไม่ใช่ค่าที่ตั้งมันจะกินจนกว่าจะใช่หรือซื้อ
-    ['Select_Fruits'] = {"Kitsune-Kitsune", "T-T", "Flame-Flame", "Ice-Ice", "Quake-Quake", "Light-Light", "Dark-Dark", "Spider-Spider", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha"} -- กินหรือซื้อตอนไม่มีผล
-}
-_G.Races_Settings = { -- ตั้งค่าเผ่า
-    ['Race'] = {
-        ['EnabledEvo'] = false,
-        ["v2"] = true,
-        ["v3"] = true,
-        ["Races_Lock"] = {
-            ["Races"] = { -- Select Races U want
-                ["Mink"] = true,
-                ["Human"] = true,
-                ["Fishman"] = true
-            },
-            ["RerollsWhenFragments"] = 20000 -- Random Races When Your Fragments is >= Settings
-        }
-    }
-}
-_G.Settings_Melee = { -- หมัดที่จะทำ
-    ['Superhuman'] = true,
-    ['DeathStep'] = true,
-    ['SharkmanKarate'] = true,
-    ['ElectricClaw'] = true,
-    ['DragonTalon'] = true,
-    ['Godhuman'] = true
-}
-_G.SwordSettings = { -- ดาบที่จะทำ
-    ['CursedDualKatana'] = true,
-    ['SharkAnchor'] = false,
-    ["Pole"] = false,
-    ['MidnightBlade'] = false,
-    ['Shisui'] = false,
-    ['Saddi'] = false,
-    ['Wando'] = false,
-    ['Yama'] = true,
-    ['Rengoku'] = false,
-    ['Canvander'] = false,
-    ['BuddySword'] = false,
-    ['TwinHooks'] = false,
-    ['HallowScryte'] = false,
-    ['TrueTripleKatana'] = false,
-    ['Saber'] = false
-}
-_G.GunSettings = { -- ปืนที่จะทำ
-    ['Kabucha'] = true,
-    ['SerpentBow'] = true,
-    ['SoulGuitar'] = true
-}
-
-getgenv().Key = "MARU-X0PX-PP2CG-PZ7M-WGD2U-BV5S"
-getgenv().id = "1161963332480286762"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-
-getgenv().PVSetting = {
-    key = 'XwkRa-FBWOK_Hobeo-', -- Secret Code
-    DelayUpdate = 300, -- Delay Update (Seconds)
-    note = 'Hobeo', 
-}
-loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua')){219206403627}
