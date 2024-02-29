56b670056c2f21256c94c058daff419ef7faed26
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-02-29 18:46:40 +0700

    Update 1.5-1.6.lua

--- 1.5-1.6.lua
+++ 1.5-1.6.lua
@@ -78,6 +78,9 @@ _G.GunSettings = { -- ปืนที่จะทำ
     ['SerpentBow'] = true,
     ['SoulGuitar'] = true
 }
+_G.SharkAnchor_Settings = {
+    ["Enabled_Farm"] = false,
+}
 
 getgenv().Key = "MARU-ZSTP-7WQNZ-SEFQ-TGGGG-GES5"
 getgenv().id = "1169895363478568984"
