f9444ad33f3b4fb60a6e70c6820ebeebbbffb384
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-03-03 21:07:35 +0700

    Update Nexus.lua

--- Nexus.lua
+++ Nexus.lua
@@ -1,127 +1,88 @@
-_G.Nexus = {
-    Main = {
-        ['Enabled'] = true,
-        ['Hop Cooldown'] = 15, -- 30 Seconds
-        ['Quests'] = {
-            ['Pull Lever'] = false,
-            ['Rainbow Haki'] = false,
-            ['Musketeer Hat'] = false,
-            ['Mirror Fractal'] = true,
-            ['Sanguine Material'] = false,
-        },
-    },
-    Sockets = {
-        Enabled = true,
-        MachineName = 'PC 1',
-        Sword = {
-            'Cursed Dual Katana',
-            'Dark Dagger',
-            'True Triple Katana',
-            'Tushita',
-            'Yama',
-            'Hallow Scythe',
-            'Saber'
-        },
-        Fruit = {
-            'Kitsune',
-            'Leopard',
-            'Dragon',
-            'Spirit',
-            'Control',
-            'Venom',
-            'Shadow',
-            'Dough',
-            'Mammoth',
-            'T-Rex'
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
+_G.Team = "Pirate" -- Marine / Pirate
+_G.KAITUN_SCRIPT = true
+_G.LogsDes = {
+    ["Enabled"] = true, -- เปิดการใช้งาน
+    ["SendAlias"] = true, -- เปิดการส่ง Alias
+    ["SendDescription"] = true, -- เปิดการส่ง Des
+    ["DelaySend"] = 5 -- วินาที
+}
+_G.MainSettings = {
+        ["EnabledHOP"] = true, -- เปิด HOP ( มันไม่มีอยู่ละใส่มาเท่ๆ )
+        ['FPSBOOST'] = true, -- ภาพกาก
+        ["FPSLOCKAMOUNT"] = 20, -- จำนวน FPS
+        ['WhiteScreen'] = true, -- จอขาว
+        ['CloseUI'] = false, -- ปิด Ui
+        ["NotifycationExPRemove"] = true, -- ลบ ExP ที่เด้งตอนฆ่ามอน
+        ['AFKCheck'] = 150, -- ถ้ายืนนิ่งเกินวิที่ตั้งมันจะรีเกม
+        ["LockFragments"] = 20000, -- ล็อคเงินม่วง
+        ["LockFruitsRaid"] = { -- ล็อคผลที่ไม่เอาไปลงดัน
+            ["Kitsune-Kitsune"] = true,
+            ["Dough-Dough"] = true,
+            ["Leopard-Leopard"] = true,
+            ["Mammoth-Mammoth"] = true,
+            ["T-T"] = true,
+            ["Dragon-Dragon"] = true
         }
-    },
-    Efficiency = {
-        ['FPS Booster'] = true,
-        ['Black Screen'] = true,
-    },
-    Fruits = {
-        ['Enabled'] = false,
-        ['Awakening Fruit'] = true,
-        ['Main'] = {
-            'Dough-Dough',
-        },
-        ['List'] = {
-            'Dark-Dark',
-            'Quake-Quake',
-            'Rumble-Rumble',
-            'Magma-Magma',
-            'Buddha-Buddha',
-        },
-    },
-    Melee = {
-        ['6 Melee'] = true,
-    },
-    Sword = {
-        ['Pole'] = false,
-        ['Saber'] = false,
-        ['Midnight Blade'] = false,
-        ['Shisui'] = false,
-        ['Saddi'] = false,
-        ['Wando'] = false,
-        ['Yama'] = true,
-        ['Tushita'] = true,
-        ['Rengoku'] = false,
-        ['Canvander'] = false,
-        ['Buddy Sword'] = false,
-        ['Spikey Trident'] = false,
-        ['Twin Hooks'] = false,
-        ['Hallow Scryte'] = false,
-        ['True Triple Katana'] = false,
-        ['Cursed Dual Katana'] = true,
-        ['Shark Anchor'] = false,
-    },
-    Gun = {
-        ['Kabucha'] = true,
-        ['Serpent Bow'] = true,
-        ['Soul Guitar'] = true
-    },
-    Race = {
-        Enabled = true,
-        Races = {
-            ['Mink'] = true,
-            ['Human'] = true,
-            ['Fishman'] = false,
-            ['Skypiea'] = true,
-        },
-        Specials = {
-            ['Ghoul'] = false,
-            ['Cyborg'] = false,
-        },
-        V4 = {
-            ['Enabled'] = false,
-        },
-    },
-    Mastery = {
-        Sword = {
-            ['Enabled'] = true,
-            ['Manual'] = true,
-            ['Sword Name'] = {
-                'Koko',
-                'Midnight Blade',
-                'True Triple Katana',
-                'Cursed Dual Katana',
-            }
-        },
-        Gun = {
-            ['Enabled'] = false,
-            ['Manual'] = true,
-            ['Gun Name'] = {
-                'Kabucha',
-                'Serpent Bow',
-                'Soul Guitar',
-            }
-        },
-        DevilFruits = true
+    }
+_G.Fruits_Settings = { -- ตั้งค่าผล
+    ['Main_Fruits'] = {'Dough-Dough'}, -- ผลหลัก ถ้ายังไม่ใช่ค่าที่ตั้งมันจะกินจนกว่าจะใช่หรือซื้อ
+    ['Select_Fruits'] = {"Kitsune-Kitsune", "T-T", "Flame-Flame", "Ice-Ice", "Quake-Quake", "Light-Light", "Dark-Dark", "Spider-Spider", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha"} -- กินหรือซื้อตอนไม่มีผล
+}
+_G.Races_Settings = { -- ตั้งค่าเผ่า
+    ['Race'] = {
+        ['EnabledEvo'] = false,
+        ["v2"] = true,
+        ["v3"] = true,
+        ["Races_Lock"] = {
+            ["Races"] = { -- Select Races U want
+                ["Mink"] = true,
+                ["Human"] = true,
+                ["Fishman"] = true
+            },
+            ["RerollsWhenFragments"] = 20000 -- Random Races When Your Fragments is >= Settings
+        }
     }
 }
+_G.Settings_Melee = { -- หมัดที่จะทำ
+    ['Superhuman'] = true,
+    ['DeathStep'] = true,
+    ['SharkmanKarate'] = true,
+    ['ElectricClaw'] = true,
+    ['DragonTalon'] = true,
+    ['Godhuman'] = true
+}
+_G.SwordSettings = { -- ดาบที่จะทำ
+    ['CursedDualKatana'] = true,
+    ['SharkAnchor'] = false,
+    ["Pole"] = false,
+    ['MidnightBlade'] = false,
+    ['Shisui'] = false,
+    ['Saddi'] = false,
+    ['Wando'] = false,
+    ['Yama'] = true,
+    ['Rengoku'] = false,
+    ['Canvander'] = false,
+    ['BuddySword'] = false,
+    ['TwinHooks'] = false,
+    ['HallowScryte'] = false,
+    ['TrueTripleKatana'] = false,
+    ['Saber'] = false
+}
+_G.GunSettings = { -- ปืนที่จะทำ
+    ['Kabucha'] = true,
+    ['SerpentBow'] = true,
+    ['SoulGuitar'] = true
+}
 
-script_key="iLnuJlovLYFAgzFalUuzNJbifJQixLHV";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenon-Trash/Nexus/main/Nexus.lua"))()
+getgenv().Key = "MARU-X0PX-PP2CG-PZ7M-WGD2U-BV5S"
+getgenv().id = "1161963332480286762"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 
 getgenv().PVSetting = {
     key = 'XwkRa-FBWOK_Hobeo-', -- Secret Code
