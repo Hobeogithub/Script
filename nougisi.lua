afffc21da59207f97cd45a2bf0809b073ac75f9b
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-03-28 12:56:47 +0700

    Create nougisi.lua

---
+++ nougisi.lua
@@ -0,0 +1,17 @@
+getgenv().Config = {
+    ["AutoReplay"] = true,
+    ["AutoJoinMatch"] = true,
+    ["SelectMap"] = "Egg Island",
+    ["AutoVoteDifficulty"] = "true",
+    ["SelectDifficulty"] = "Nightmare",
+    ["SelectMacro"] = "toiletspregg",
+    ["AutoSkip"] = true,
+    ["DeleteMap"] = true,
+    ["PlayMacro"] = true,
+    ["AutoSave"] = true,
+    ["BoostFPS"] = true,
+    ["AutoClaimQuest"] = true,
+    ["WhiteScreen"] = true,
+    ["DelayJoin"] = "30",
+}
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
