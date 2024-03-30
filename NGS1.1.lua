749887b474b7b1026d5850a82fca693fbc965db6
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-03-31 03:42:51 +0700

    Update NGS1.1.lua

--- NGS1.1.lua
+++ NGS1.1.lua
@@ -15,8 +15,9 @@ getgenv().Config = {
     ["AutoClaimQuest"] = true,
     ["AutoClaimEasterQuest"] = true,
     ["AutoClaimEventsPass"] = true,
-    ["WhiteScreen"] = true,
-    ["DelayJoin"] = "15",
+    ["AutoClaimVIPRewards"] = true,
+    ["WhiteScreen"] = false,
+    ["DelayJoin"] = "30",
     ["PlacingFailsafe"] = true,
     ["ExchangeTier"] = "3",
     ["AutoExchangeEgg"] = "true",
@@ -24,6 +25,12 @@ getgenv().Config = {
     ["RequiredGem"] = 1000,
     ["AutoMail"] = true,
     ["WH_MatchComplete"] = true,
+    ["AutoUseBoost"] = true,
+    ["UseBoosts"] = {
+        ["2xCoinsBoost"] = true,
+        ["2xEggsBoost"] = true,
+    },
+    ["UseAll"] = false,
     ["WebhookURL"] = "https://discord.com/api/webhooks/1220058813726593054/jFB1xABqHM4uxeUa9yRkFoWYbiQSqz_O6oMClPLuFHqCgVGw1wF0TkrjCcy1dk4Ibe8K",
 }
 loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
