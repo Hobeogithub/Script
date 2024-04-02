ef562cfd1b1f46ad7e498abe9fbd093e4abd5ae0
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-04-02 14:01:41 +0700

    Update NGS2.5.lua

--- NGS2.5.lua
+++ NGS2.5.lua
@@ -1,7 +1,7 @@
 getgenv().Key = "k0a814c5234e1b3fe5ef8389"
 getgenv().Config = {
     ["AutoReplay"] = true,
-    ["AutoJoinMatch"] = true,
+    ["AutoJoinMatch"] = false,
     ["TPLobbyIfPlayer"] = true,
     ["SelectMap"] = "Egg Island",
     ["AutoVoteDifficulty"] = "true",
@@ -16,7 +16,7 @@ getgenv().Config = {
     ["AutoClaimEasterQuest"] = true,
     ["AutoClaimEventsPass"] = true,
     ["AutoClaimVIPRewards"] = true,
-    ["WhiteScreen"] = false,
+    ["WhiteScreen"] = true,
     ["DelayJoin"] = "30",
     ["PlacingFailsafe"] = true,
     ["ExchangeTier"] = "3",
