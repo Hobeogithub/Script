4bfdb2820fff57bea6413f737db3150521815904
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-04-04 03:06:59 +0700

    Update NGS3.5.lua

--- NGS3.5.lua
+++ NGS3.5.lua
@@ -1,6 +1,6 @@
 getgenv().Key = "ke722fae319d8c8ca8595862"
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/888605470217080856/1224936005891784784/loisprite.json?ex=661f4d8e&is=660cd88e&hm=ce839fa49951bb6c8f7b7cd8e97c96b398b52d8224fa04dd571905b9801dead6&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1224650126518124585/1224650159942668340/MacroEggNGS.json?ex=661e4357&is=660bce57&hm=c5c208a7b53cc5310d56f4cdbf0cd30aec61ac89ada09d32eb280cf6098f7be6&"
 getgenv().Config = {
     ["AutoReplay"] = true,
     ["AutoJoinMatch"] = true,
@@ -8,7 +8,7 @@ getgenv().Config = {
     ["SelectMap"] = "Egg Island",
     ["AutoVoteDifficulty"] = "true",
     ["SelectDifficulty"] = "Nightmare",
-    ["SelectMacro"] = "loisprite",
+    ["SelectMacro"] = "MacroEggNGS",
     ["AutoSkip"] = true,
     ["DeleteMap"] = true,
     ["PlayMacro"] = true,
