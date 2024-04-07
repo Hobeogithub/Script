3e9297712c1bd5ac01fdbaa3e24a1bd8da36e5e6
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-04-07 20:58:33 +0700

    Create Solfarm.lua

---
+++ Solfarm.lua
@@ -0,0 +1,26 @@
+_G.Kaitan = {
+    Farming = {
+        ['Enabled'] = true,
+        ['Crafted Gloves'] = "Exo Gauntlet",
+        ['Activated Glove'] = "Exo Gauntlet",
+        ['Activated Potions'] = {"Lucky Potion", "Speed Potion"}
+    },
+    Performance = {
+        ['FPS Booster'] = true,
+        ['FPS Limit'] = "15",
+        ['Disabled VFX'] = true,
+        ['Black Screen'] = true,
+        ['Close Interface'] = true
+    },
+    Webhooks = {
+        ['Enabled'] = false,
+        ['Enter URL'] = ""
+    },
+    Settings = {
+        ['Upgrade Storage'] = false,
+        ['Streamer Mode'] = true,
+        ['FullBright'] = false,
+        ['Remove Fog'] = true,
+    }
+}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua')){278080594532}
