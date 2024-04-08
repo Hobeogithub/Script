6ffc27bc0c5d5c8815c5672ad225c77c42014982
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-04-09 00:51:36 +0700

    Create NGS5.7-6.5.lua

---
+++ NGS5.7-6.5.lua
@@ -0,0 +1,39 @@
+getgenv().Key = "ke722fae319d8c8ca8595862"
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1224650126518124585/1225597546098069504/MacroEggBunny.json?ex=6621b5a9&is=660f40a9&hm=d40576b6420be9283aeb3b16513d71d02ac0134978626b0555b5973f9f06e772&"
+getgenv().Config = {
+    ["AutoReplay"] = true,
+    ["AutoJoinMatch"] = true,
+    ["TPLobbyIfPlayer"] = true,
+    ["SelectMap"] = "Egg Island",
+    ["AutoVoteDifficulty"] = "true",
+    ["SelectDifficulty"] = "Nightmare",
+    ["SelectMacro"] = "MacroEggBunny",
+    ["AutoSkip"] = true,
+    ["DeleteMap"] = true,
+    ["PlayMacro"] = true,
+    ["AutoSave"] = true,
+    ["BoostFPS"] = true,
+    ["AutoClaimQuest"] = true,
+    ["AutoClaimEasterQuest"] = true,
+    ["AutoClaimEventPass"] = true,
+    ["AutoClaimVIP"] = true,
+    ["WhiteScreen"] = true,
+    ["DelayJoin"] = "45",
+    ["JoinFailsafe"] = false,
+    ["PlacingFailsafe"] = true,
+    ["ExchangeTier"] = "1",
+    ["AutoExchangeEgg"] = "true",
+    ["MailName"] = "",
+    ["RequiredGem"] = 1000,
+    ["AutoMail"] = true,
+    ["WH_MatchComplete"] = true,
+    ["AutoUseBoost"] = true,
+    ["UseBoosts"] = {
+        ["2xCoinsBoost"] = true,
+        ["2xEggsBoost"] = true,
+    },
+    ["UseAll"] = true,
+    ["WebhookURL"] = "https://discord.com/api/webhooks/1226952562398462056/jkauN5l0ZJRaZvF1_L7H8C89z8t4tlOBld_IxTuLN6YJ8cD8dUA1v8RSnzTcKpK3e_TN",
+}
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
