fc4a1b73de59574dfe1272d743d2408ce5100764
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-04-13 00:22:14 +0700

    Update NGS2.3.lua

--- NGS2.3.lua
+++ NGS2.3.lua
@@ -25,14 +25,14 @@ getgenv().Config = {
     ["AutoExchangeEgg"] = "false",
     ["MailName"] = "",
     ["RequiredGem"] = 1000,
-    ["AutoMail"] = true,
+    ["AutoMail"] = false,
     ["WH_MatchComplete"] = true,
     ["AutoUseBoost"] = true,
     ["UseBoosts"] = {
         ["2xCoinsBoost"] = true,
         ["2xEggsBoost"] = true,
     },
-    ["UseAll"] = true,
+    ["UseAll"] = false,
     ["WebhookURL"] = "https://discord.com/api/webhooks/1212449694350049330/6vrOV79kMOSTtm4GuhBSOib2u6fT3Gxuy3eSOvGyafVe3H2YR1hRN7RvOTvK1_zVuCy0",
 }
 loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
