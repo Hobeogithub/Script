0549c3f423422a3cc3a51c77455d9093e8a84edc
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-04-13 00:24:43 +0700

    Update NGS5.7-6.5.lua

--- NGS5.7-6.5.lua
+++ NGS5.7-6.5.lua
@@ -25,14 +25,14 @@ getgenv().Config = {
     ["AutoExchangeEgg"] = "false",
     ["MailName"] = "",
     ["RequiredGem"] = 1000,
-    ["AutoMail"] = true,
+    ["AutoMail"] = false,
     ["WH_MatchComplete"] = true,
     ["AutoUseBoost"] = true,
     ["UseBoosts"] = {
         ["2xCoinsBoost"] = true,
         ["2xEggsBoost"] = true,
     },
-    ["UseAll"] = true,
+    ["UseAll"] = false,
     ["WebhookURL"] = "https://discord.com/api/webhooks/1226952562398462056/jkauN5l0ZJRaZvF1_L7H8C89z8t4tlOBld_IxTuLN6YJ8cD8dUA1v8RSnzTcKpK3e_TN",
 }
 loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
