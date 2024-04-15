737f50b4b69c2a07cea4170ae2ede324f0f214e3
Hobeogithub <158523582+Hobeogithub@users.noreply.github.com>
2024-04-15 23:28:16 +0700

    Delete duphong.lua

--- duphong.lua
+++
@@ -1,6 +0,0 @@
-getgenv().PVSetting = {
-    key = 'XwkRa-FBWOK_Hobeo-', -- Secret Code
-    DelayUpdate = 15, -- Delay Update (Seconds)
-    note = 'Hobeo', 
-}
-loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
